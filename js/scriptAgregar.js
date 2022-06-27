8b284aa1cf0dd162be3bfdb98031b65b4b258a93
wilberth <wgcampos15@gmail.com>
2022-06-27 02:02:49 -0500

    login agregar register

--- js/scriptAgregar.js
+++ js/scriptAgregar.js
@@ -1,8 +1,13 @@
 // FORM
 //variables inputs guardados
 const inputPlacaMadre = document.getElementById("placaMadre");
-
-
+const inputProcesador = document.getElementById("procesador");
+const inputTarjetaDeVideo = document.getElementById("tarjetaDeVideo");
+const inputFuenteDePoder = document.getElementById("fuenteDePoder");
+const inputAlmacenamiento = document.getElementById("almacenamiento");
+const inputRam= document.getElementById("ram");
+const inputGabinete= document.getElementById("gabinete");
+const inputimg = document.getElementById("file");
 
 
 
@@ -16,15 +21,65 @@ const button = document.getElementById('add');
 console.log(button); // 👉️ button#btn
 
 const agregar = ()=>{
-    let valorPlacaMadre= inputPlacaMadre.value;
+    var valorPlacaMadre= inputPlacaMadre.value;
+    var valorInputProcesador = inputProcesador.value;
+    var valorTarjetaDeVideo = inputTarjetaDeVideo.value;
+    var valorFuenteDePoder = inputFuenteDePoder.value;
+    var valorAlmacenamiento = inputAlmacenamiento.value;
+    var valorRam = inputRam.value;
+    var valorGabinete = inputGabinete.value;
+    var valorFile = inputimg.value;
+    
 
     if (valorPlacaMadre=="") {
         inputPlacaMadre.focus();
-       alerta();
+        alerta();
        
-        
-    } else {
+    
 
+    } 
+    if (valorInputProcesador=="") {
+        inputProcesador.focus();
+        alerta();
+        
+    } 
+
+    if (valorTarjetaDeVideo=="") {
+        inputTarjetaDeVideo.focus();
+        alerta();
+        
+    }
+    if (valorFuenteDePoder=="") {
+        inputFuenteDePoder.focus();
+        alerta();
+        
+    }
+    if (valorAlmacenamiento=="") {
+        inputAlmacenamiento.focus();
+        alerta();
+        
+    }
+    if (valorRam=="") {
+        inputRam.focus();
+        alerta();
+        
+    }
+    if (valorGabinete=="") {
+        inputGabinete.focus();
+        alerta();
+        
+    }
+    if (valorFile=="") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'No se ha agregado una imagen',
+           
+          })
+        
+        
+    }
+    if (valorPlacaMadre!=""&& valorInputProcesador!=""&&valorTarjetaDeVideo!=""&&valorFuenteDePoder!=""&&valorAlmacenamiento!=""&&valorRam!=""&&valorGabinete!=""&&valorFile!="") {
         Swal.fire({
             title: 'Guardado',
             text: 'Tu pc ha sido agregada',
@@ -33,8 +88,8 @@ const agregar = ()=>{
             imageHeight: 200,
             imageAlt: 'Custom image',
           })
-        
     }
+    
     
 
     
