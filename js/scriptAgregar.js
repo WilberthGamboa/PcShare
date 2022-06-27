4e96922d01dfbd5d0e92c9ffc7a62a82c424c54e
wilberth <wgcampos15@gmail.com>
2022-06-26 22:38:48 -0500

    add alert

--- js/scriptAgregar.js
+++ js/scriptAgregar.js
@@ -1,18 +1,57 @@
+// FORM
+//variables inputs guardados
+const inputPlacaMadre = document.getElementById("placaMadre");
+
+
+
+
+
+
+
+
+
+
+
 const button = document.getElementById('add');
 console.log(button); // 👉️ button#btn
 
 const agregar = ()=>{
-    Swal.fire({
-        title: 'Guardado',
-        text: 'Tu pc ha sido agregada',
-        imageUrl: 'https://unsplash.it/400/200',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-      })
+    let valorPlacaMadre= inputPlacaMadre.value;
+
+    if (valorPlacaMadre=="") {
+        inputPlacaMadre.focus();
+       alerta();
+       
+        
+    } else {
+
+        Swal.fire({
+            title: 'Guardado',
+            text: 'Tu pc ha sido agregada',
+            imageUrl: 'https://unsplash.it/400/200',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+        
+    }
+    
+
+    
 }
 
 button.onclick=agregar;
+
+const alerta = ()=>{
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El campo no puede estar vacio',
+       
+      })
+}
+
+
 
 
 
