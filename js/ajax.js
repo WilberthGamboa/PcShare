e500ee9a10c1df3063544b038bc409de5c9d5645
Your Name~ <wgcampos15@gmail.com>
2022-07-09 18:02:57 -0500

    escribir en documento y se actualiza

--- js/ajax.js
+++ js/ajax.js
@@ -5,11 +5,23 @@ prueba.onclick = () =>{
 }
 
 function generar(hola) {
-
+    if (document.getElementById("contenido").hasChildNodes()) {
+       
+        while (document.getElementById("contenido").firstChild) {
+            //The list is LIVE so it will re-index each call
+            document.getElementById("contenido").removeChild(document.getElementById("contenido").firstChild);
+        }
+        
+    }
    // hola=JSON.stringify(hola);
     hola.forEach(item => {
-        console.log(item[0].nombre);
-       document.getElementById("contenido").innerHTML =""+item[0].nombre;
+       // console.log(item);
+       const div = document.createElement("p"); // <div></div>
+       div.textContent = item[0].id+item[0].nombre+item[0].procesador;
+div.id = "page";          // <div id="page"></div>
+div.className = "data";   // <div id="page" class="data"></div>
+div.style = "color: red"; // <div id="page" class="data" style="color: red"></div>
+       document.getElementById("contenido").appendChild(div);
         
     });
 
