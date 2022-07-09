92e77231e1d2237fe799f74969b997cb2fbbfa40
Ariel Fernández <88345754+ArielFdz@users.noreply.github.com>
2022-07-09 18:31:06 -0500

    Add files via upload

---
+++ php/database2.php
@@ -0,0 +1,12 @@
+<?php 
+
+$user="root";
+$password="";
+
+try {
+    $connection = new PDO("mysql:host=localhost;dbname=usuarios;", $user, $password);
+    $connection ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+  }
+
+?>
