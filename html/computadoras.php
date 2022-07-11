62945fb1b48daf3a33450cfb03f6507c4c8d78c9
Ariel Fernández <88345754+ArielFdz@users.noreply.github.com>
2022-07-11 04:23:53 -0500

    Update computadoras.php

--- html/computadoras.php
+++ html/computadoras.php
@@ -4,10 +4,7 @@
   require '../php/database.php';
   // $xd = valor js
   if (isset($_SESSION['user_id'])) {
- //   $sql= SELECT nombre,placaMadre,procesador,tarjetaDeVideo,fuenteDePoder,almacenamiento,ram,gabinete,imagen FROM computadoras;
-
- //   echo "<H1>  {$_GET['busca']} </H1> ";
- //NO MOVER XD
+ 
 $condicion= $_GET['busca'];
  $query = "SELECT id,nombre,placaMadre,procesador,tarjetaDeVideo,fuenteDePoder,almacenamiento,ram,gabinete FROM computadoras WHERE computadoras.nombre like '%$condicion%'";
  
@@ -24,22 +21,7 @@ $condicion= $_GET['busca'];
 
  header('Content-Type: application/json');
 echo json_encode($userData);
-/*
 
- $sql = "SELECT nombre FROM computadoras"; 
- $query = $conn -> prepare($sql); 
- $query -> execute(); 
- $results = $query -> fetchAll(PDO::FETCH_OBJ); 
-  json_encode($results);
- /*
- if($query -> rowCount() > 0)   { 
-    foreach($results as $result) { 
-        $hola = $result -> nombre;
-    echo $hola ;
-    
-       }
-     }
-*/
 }else{
     header('Location: login.php');
   }
